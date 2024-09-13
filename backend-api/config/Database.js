5db743d50bee67eea762888fd20d3676c4aa71ef
Duc Tran <tranduc2004nd01@gmail.com>
2024-09-13 16:02:55 +0700

    connect database

--- backend-api/config/Database.js
+++ backend-api/config/Database.js
@@ -1,8 +1,16 @@
-import {Sequelize} from "sequelize";
+import { user, password, host, database, port } from './dbConfig.js';
+import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('test','root','',{
-    host: "localhost",
-    dialect: "mysql"
+export const db = new Sequelize(database, user, password, {
+  host: host,
+  dialect: 'mysql',
 });
 
-export default db;
+export const connection = async () => {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
--- backend-api/models/User.js
+++ backend-api/models/User.js
@@ -1,23 +1,27 @@
-import { Sequelize } from "sequelize";
-import db from "../config/Database.js";
+import { Sequelize } from 'sequelize';
+import { db } from '../config/Database.js';
 
 const { DataTypes } = Sequelize;
 
-const User = db.define('users',{
-    name:{
-        type: DataTypes.STRING
+const User = db.define(
+  'users',
+  {
+    name: {
+      type: DataTypes.STRING,
     },
-    email:{
-        type: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
     },
-    password:{
-        type: DataTypes.STRING
+    password: {
+      type: DataTypes.STRING,
     },
-    refresh_token:{
-        type: DataTypes.TEXT
-    }
-},{
-    freezeTableName:true
-});
+    refresh_token: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
 export default User;
