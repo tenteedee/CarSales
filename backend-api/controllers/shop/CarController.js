ac1ab7680641640a47cc0679f83d0b0bf9a9fc77
Duc Tran <tranduc2004nd01@gmail.com>
2024-09-19 00:47:32 +0700

    Add api for Car: create, delete by id, update by id

--- backend-api/controllers/shop/CarController.js
+++ backend-api/controllers/shop/CarController.js
@@ -2,6 +2,29 @@ import Car from '../../models/Car.js';
 import CarImage from '../../models/CarImage.js';
 import { Op } from 'sequelize';
 
+export const getAllCars = async (req, res) => {
+  try {
+    const cars = await Car.findAll({
+      attributes: ['id', 'model', 'brand', 'type', 'price', 'stock'],
+      include: [
+        {
+          model: CarImage,
+          as: 'images',
+          attributes: ['image_url'],
+        },
+      ],
+    });
+
+    if (!cars || cars.length === 0) {
+      return res.status(404).json({ message: 'No cars found!' });
+    }
+
+    res.status(200).json(cars);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 export const getCarById = async (req, res) => {
   try {
     const carId = req.params.id;
@@ -87,6 +110,83 @@ export const filterCars = async (req, res) => {
     }
 
     res.status(200).json(cars);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createNewCar = async (req, res) => {
+  try {
+    const { model, brand, type, price, description, stock, images } = req.body;
+
+    const existingCar = await Car.findOne({ where: { model, brand, type } });
+    if (existingCar) {
+      return res
+        .status(400)
+        .json({ error: 'Car already exists in the system!' });
+    }
+
+    const newCar = await Car.create({
+      model,
+      brand,
+      type: type.toLowerCase(),
+      price,
+      description,
+      stock,
+    });
+
+    if (images && images.length > 0) {
+      for (const image of images) {
+        await CarImage.create({
+          car_id: newCar.id,
+          image_url: image,
+        });
+      }
+    }
+
+    res.status(201).json({ message: 'Car created successfully!' });
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const deleteCar = async (req, res) => {
+  try {
+    const carId = req.params.id;
+    const car = await Car.findByPk(carId);
+
+    if (!car) {
+      return res.status(404).json({ message: 'Car not found' });
+    } else {
+      await car.destroy();
+    }
+
+    res.status(200).json({ message: 'Car deleted successfully!' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateCar = async (req, res) => {
+  try {
+    const carId = req.params.id;
+    const { model, brand, type, price, stock } = req.body;
+
+    const car = await Car.findByPk(carId);
+
+    if (!car) {
+      return res.status(404).json({ message: 'Car not found' });
+    }
+
+    car.model = model || car.model;
+    car.brand = brand || car.brand;
+    car.type = type || car.type;
+    car.price = price || car.price;
+    car.stock = stock || car.stock;
+
+    await car.save();
+
+    res.status(200).json({ message: 'Car updated successfully', car });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
