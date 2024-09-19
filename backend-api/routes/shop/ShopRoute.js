cd9811bea5b2a18b3c20e317ec800283c89ce679
Vu Tung Duy <duyvthe180740@fpt.edu.vn>
2024-09-19 09:50:34 +0700

    update controller

--- backend-api/routes/shop/ShopRoute.js
+++ backend-api/routes/shop/ShopRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import authRouter from './AuthRoute.js';
 import carRouter from './CarRoute.js';
+import {getAllSettings} from "../../controllers/shop/SettingController.js";
 
 const router = express.Router();
 router.use('/auth', authRouter);
 router.use('/car', carRouter);
-
+router.get('/settings', getAllSettings);
 export default router;
