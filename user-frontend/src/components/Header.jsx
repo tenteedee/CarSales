94f4a33e7722c710ca374c0853986b5b937b8f98
Thuc Anh <nguyenthucanh0004@gmail.com>
2024-09-24 11:37:16 +0700

    language Update

--- user-frontend/src/components/Header.jsx
+++ user-frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { fetchConfig } from '../reduxStore/configSlice';
 import Loading from './Loading';
 import { setLogin } from '../reduxStore/authSlice';
+import { useTranslation } from 'react-i18next'; // Import useTranslation
 
 const Header = () => {
     const config = useSelector((state) => state.config.config);
@@ -11,15 +12,19 @@ const Header = () => {
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(true); // Add loading state
     const [isOpen, setIsOpen] = useState(false); // Add state for dropdown open/close
+    const { t, i18n } = useTranslation(); // Correctly use useTranslation
+
+    const handleChangeLanguage = (lang) => {
+        i18n.changeLanguage(lang); // No type declaration needed in JSX
+    };
 
     useEffect(() => {
         const fetchData = async () => {
-            setLoading(true); // Stop loading when done
+            setLoading(true);
             await dispatch(fetchConfig());
-        };
-        fetchData().then(() => {
             setLoading(false); // Stop loading when done
-        });
+        };
+        fetchData();
     }, [dispatch]);
 
     const logOut = async () => {
@@ -41,66 +46,70 @@ const Header = () => {
     }
 
     return (
-        <>
-            <header className='b-topBar'>
-                <div
-                    className='container wow slideInDown'
-                    data-wow-delay='0.7s'
-                >
-                    <div className='row'>
-                        <div className='col-md-4 col-xs-6'>
-                            <div className='b-topBar__addr'>
-                                <span className='fa fa-map-marker'></span>
-                                {config?.address}
-                            </div>
+        <header className="b-topBar">
+            <div className="container wow slideInDown" data-wow-delay="0.7s">
+                <div className="row" style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className="col-md-3 col-xs-6">
+                        <div className="b-topBar__addr">
+                            <span className="fa fa-map-marker"></span>
+                            {config?.address}
                         </div>
-                        <div className='col-md-2 col-xs-6'>
-                            <div className='b-topBar__tel'>
-                                <span className='fa fa-phone'></span>
-                                {config?.phone}
-                            </div>
+                    </div>
+                    <div className="col-md-3 col-xs-6">
+                        <div className="b-topBar__tel">
+                            <span className="fa fa-phone"></span>
+                            {config?.phone}
                         </div>
-                        <div className='col-md-2 col-xs-6'></div>
+                    </div>
+                    <div className="col-md-3 col-xs-6">
                         {!token ? (
-                            <>
-                                <div className='col-md-4 col-xs-6'>
-                                    <nav className='b-topBar__nav'>
-                                        <ul>
-                                            <li>
-                                                <Link to={'/login'}>Login</Link>
-                                            </li>
-                                            <li>
-                                                <Link to={'/signup'}>Register</Link>
-                                            </li>
-                                            <li>
-                                                <Link to={'/cart'}>Cart</Link>
-                                            </li>
-                                        </ul>
-
-                                    </nav>
-                                </div>
-                            </>
+                            <nav className="b-topBar__nav">
+                                <ul style={{ display: 'flex', gap: '10px' }}>
+                                    <li><Link to={'/login'}>Login</Link></li>
+                                    <li><Link to={'/signup'}>Register</Link></li>
+                                    <li><Link to={'/cart'}>Cart</Link></li>
+                                </ul>
+                            </nav>
                         ) : (
-                            <>
-                                <div className='col-md-4 col-xs-6'>
-                                    <nav className='b-topBar__nav'>
-                                        <ul>
-                                            <li>
-                                                <Link onClick={logOut} to={"/"}>Đăng Xuất</Link>
-                                            </li>
-                                            <li>
-                                                <Link to={'/profile'}>Cá nhân</Link>
-                                            </li>
-                                        </ul>
-
-                                    </nav>
-                                </div>
-                            </>
+                            <nav className="b-topBar__nav">
+                                <ul style={{ display: 'flex', gap: '10px' }}>
+                                    <li>
+                                        <Link onClick={logOut} to={'/'}>{t('LOGOUT')}</Link>
+                                    </li>
+                                    <li>
+                                        <Link to={'/profile'}>{t('PROFILE')}</Link>
+                                    </li>
+                                </ul>
+                            </nav>
                         )}
                     </div>
+                    <div className="col-md-2 col-xs-6">
+                        <div className="b-topBar__lang">
+                            <div className="dropdown">
+                                <a href="#" className="dropdown-toggle" data-toggle="dropdown">NGÔN NGỮ</a>
+                                <a className="m-langLink dropdown-toggle" data-toggle="dropdown" href="#">
+                                    <span className="b-topBar__lang-flag m-en"></span>VN<span className="fa fa-caret-down"></span>
+                                </a>
+                                <ul className="dropdown-menu h-lang">
+                                    <li>
+                                        <a className="m-langLink dropdown-toggle" data-toggle="dropdown" href="#">
+                                            <span className="b-topBar__lang-flag m-en"></span>VN
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="m-langLink dropdown-toggle" data-toggle="dropdown" href="#">
+                                            <span className="b-topBar__lang-flag m-es"></span>EN
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </header>
-        </>
+            </div>
+        </header>
+
+
     );
 };
 
---
+++ user-frontend/src/translations/vn.js
@@ -0,0 +1,8 @@
+const global_vn = {
+    LANGUAGE: 'Ngôn ngữ',
+    LOGOUT: 'Đăng xuất',
+    PROFILE: 'Cá nhân',
+    // Add more translations here
+};
+
+export default global_vn;
